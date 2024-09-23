530f8344d9c08db2d5095b9ed87d58523c1ff469
Kim <kimjy2000117@gmail.com>
2024-09-23 19:41:07 +0900

    ✨ 폼태그 레이아웃 스타일 추가
    폼태그 레이아웃 스타일 추가

---
+++ src/style/formLayout.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+    //레이아웃
+    width: 100%;
+`;
+
+export const FormLayout = styled.div`
+    //레이아웃
+    width: 100%;
+    height: 50px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-bottom: 13px;
+`;
+
+export const TitleLayout = styled.div`
+    //레이아웃
+    width: 100%;
+    display: flex;
+    text-align: start;
+    margin-bottom: 5px;
+
+    //폰트
+    font-size: 20px;
+    font-weight: 700;
+`;
