f34fe6723a719bcd3eef03be547d5762b0e0f60c
Kim <kimjy2000117@gmail.com>
2024-09-23 19:30:14 +0900

    🔨 경로 추가
    회원가입 및 비밀번호 변경 완료 페이지 경로 추가

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,13 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from '../pages/login/Login';
+import SignUp from '../pages/signUp/SignUp';
+import SignUpComplete from '../pages/signUp/SignUpComplete';
 import FindPw from '../pages/findPw/FindPw';
+import FindPwComplete from '../pages/findPw/FindPwComplete';
 
 export const Router = () => {
     return(
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Login />} />
+                <Route path='/signUp' element={<SignUp />} />
+                <Route path='/signUpComplete' element={<SignUpComplete/>} />
                 <Route path='/findPw' element={<FindPw />} />
+                <Route path='/findPwComplete' element={<FindPwComplete/>} />
             </Routes>
         </BrowserRouter>
     );
