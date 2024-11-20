e21f38720b3190de4d5945f346cbcee055976192
Kim <kimjy2000117@gmail.com>
2024-11-20 22:55:03 +0900

    🐛 토큰값 에러 수정
    localStorage에 "Bearer 토큰값"형태로 저장되어 서버측으로 토큰값에 "Bearer"을 한번 더 붙여주어 에러가 발생함에 따라 토큰값 저장하는 로직 수정

--- src/components/login/Form.jsx
+++ src/components/login/Form.jsx
@@ -3,11 +3,14 @@ import { useInput } from "../../hooks/useInput";
 import { LargeButton } from "../buttons/LargeButton";
 import { isMinLength, isMaxLength, isEmpty, isEmail} from "../../utils/validation"
 import { useState } from "react";
-import axiosInstance from "../../api/axiosInstance";
+import axiosInstance from "../../api/nonAuth/axiosInstance";
 
 export default function Form(){
     // 에러가 발생했을 때 애니메이션을 트리거하기 위한 상태
     const [submitAttempt, setSubmitAttempt] = useState(false);
+
+    //아이디 혹은 비밀번호 일치 여부
+    const [loginError, setLoginError] = useState(false);
 
     const {
         value: emailValue,
@@ -31,12 +34,13 @@ export default function Form(){
             const response = await axiosInstance.post('/login', requestData);
 
             const accessToken = response.headers['authorization']
-            localStorage.setItem('accessToken', `Bearer ${accessToken}`);
+            localStorage.setItem('accessToken', `${accessToken}`);
 
             alert("로그인 성공");
           } catch (error) {
             console.error('API 요청 오류:', error);
-            alert(error.response.data.message);
+            //로그인 검증 실패
+            setLoginError(true);
           }
     };
 
@@ -65,10 +69,11 @@ export default function Form(){
                 onChange={(e) => {
                     emailInputHandler(e);
                     setSubmitAttempt(false); // 다시 입력하면 흔들림 상태 리셋
+                    setLoginError(false); // 다시 입력하면 에러 상태 리셋
                 }}
-                error={emailHasError || (submitAttempt && isEmpty(emailValue)) ? '※정확한 이메일 아이디를 입력하세요.' : null}
+                error={emailHasError || (submitAttempt && isEmpty(emailValue)) ? '※정확한 이메일 아이디를 입력하세요.' : loginError ? '※이메일을 다시 확인해주세요.' : null}
                 borderColor={emailHasError || (submitAttempt && isEmpty(emailValue)) ?  '#f31d1d' : null}
-                shake={submitAttempt && (emailHasError || isEmpty(emailValue))}
+                shake={(submitAttempt && (emailHasError || isEmpty(emailValue))) || loginError}
             />
 
             <Input
@@ -79,10 +84,11 @@ export default function Form(){
                 onChange={(e) => {
                     passwordInputHandler(e);
                     setSubmitAttempt(false); // 다시 입력하면 흔들림 상태 리셋
+                    setLoginError(false); // 다시 입력하면 에러 상태 리셋
                 }}
-                error = {passwordHasError || (submitAttempt && isEmpty(passwordValue)) ? '※비밀번호는 8~15자리여야 합니다.' : null}
+                error = {passwordHasError || (submitAttempt && isEmpty(passwordValue)) ? '※비밀번호는 8~15자리여야 합니다.' : loginError ? '※비밀번호를 다시 확인해주세요.' : null}
                 borderColor={passwordHasError || (submitAttempt && isEmpty(passwordValue)) ? '#f31d1d' : null}
-                shake={submitAttempt && (passwordHasError || isEmpty(passwordValue))}
+                shake={(submitAttempt && (passwordHasError || isEmpty(passwordValue))) || loginError}
             />
 
             <LargeButton 
