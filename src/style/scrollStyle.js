84023bcd6509e1e0167158ec27df2be2afe99f75
Kim <kimjy2000117@gmail.com>
2024-11-23 17:25:35 +0900

    ✨ 스크롤 스타일 추가
    스크롤 스타일 추가

---
+++ src/style/scrollStyle.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const ScrollStyle = styled.div`
+    height: 70%;
+    /* padding-right: 10px; */
+
+    overflow-y: auto;  // 내용이 넘칠 때 세로 스크롤 추가
+    overflow-x: hidden;
+    flex: 1; /* 남은 공간을 스크롤 영역으로 확장 */
+
+
+    /* 스크롤바 스타일링 */
+    &::-webkit-scrollbar {
+        width: 3px;  /* 스크롤바의 너비 */
+    }
+
+    &::-webkit-scrollbar-thumb {
+        background-color: #d1d1d1;  /* 스크롤바의 색상 */
+        border-radius: 4px;
+    }
+
+    &::-webkit-scrollbar-thumb:hover {
+        background-color: #b8b8b8;  /* 마우스를 올렸을 때 색상 */
+    }
+`;
