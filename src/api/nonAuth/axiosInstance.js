adda79f684ea4e7ce88bd3e684b5dca196bee54f
Kim <kimjy2000117@gmail.com>
2025-01-31 23:47:41 +0900

    🔧 Host 변경
    ec2 인스턴스 host로 변경

--- src/api/nonAuth/axiosInstance.js
+++ src/api/nonAuth/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "http://3.36.151.72:8080/api",
     // timeout: 10000,
     withCredentials: true,
 });
